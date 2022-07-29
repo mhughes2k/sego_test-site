70f27f11226fbd4a93c64b1428e1561fafaea678
Michael Hughes <michael@phoenixproductions.org.uk>
2022-07-29 12:52:27 +0100

    Demo code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function myFunction(event) {
+function myFunctionA(event) {
 
     event.preventDefault();
     console.log(event);
 
     document.getElementById('form').style.display ='none';
     document.getElementById('wrap').style.display = 'block';
-
+}
 function myFunction(showStuff) {
-
+    showStuff.preventDefault();
     // This works but shows all the images
 
         // let divs = document.getElementById('wrap');
@@ -17,14 +17,18 @@ function myFunction(showStuff) {
 
 
     // This doesn't work and should show only product_img_2 and hide product_img_3 when introvert is chosen
-        let divs = document.querySelectorAll('.product_grid,.column_2,.product_img_2');
-        if (persona.value == 'introvert') {
-
-            divs.style.display = 'block';
-            document.querySelector('.product_img_3').style.display='none';
-            
-
-           }
-            
-        }
+    document.getElementById('form').style.display ='none';
+    
+    let dogs = document.querySelectorAll('.product_grid');
+    dogs.forEach(e => {e.style.display ='none'});
+    
+    if (personalbar.value != "indifferent") {
+        let divs = document.querySelectorAll("[data-personality='" + persona.value + "']");
+        console.log("[data-personality='" + persona.value + "']");
+        divs.forEach(div => {
+            div.style.display = 'block';
+        })
     }
+    
+            
+}
